609f4cb4f15b1ea2cede67d81e009991d2a5d184
Ryan Whittingham <ryanwhittingham89@gmail.com>
2021-12-27 21:45:10 -0800

    move basic db querying into own module

--- jvm/src/main/scala/web/db/Db.scala
+++ jvm/src/main/scala/web/db/Db.scala
@@ -1,6 +1,33 @@
 package com.ryanwhittingham.web.db
 
-object Init {
-  def initDb(): Unit = {}
+import com.ryanwhittingham.web.config.Config.config
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import io.getquill._
+
+object Db {
+  // Initialize db connection
+  val pgDataSource = new org.postgresql.ds.PGSimpleDataSource()
+  pgDataSource.setUser(config.dbUser)
+  pgDataSource.setDatabaseName(config.dbName)
+  pgDataSource.setPortNumber(config.dbPort)
+
+  if (!config.dbPassword.trim.isEmpty) {
+    pgDataSource.setPassword(config.dbPassword)
+  }
+
+  val dbConfig = new HikariConfig()
+  dbConfig.setDataSource(pgDataSource)
+
+  val ctx = new PostgresJdbcContext(LowerCase, new HikariDataSource(dbConfig))
+
+  case class Blog(
+      id: Int,
+      tstamp: Int,
+      title: String,
+      author: String,
+      contents: String,
+      org_hash: String
+  )
 
 }
--- jvm/src/main/scala/web/query/Blog.scala
+++ jvm/src/main/scala/web/query/Blog.scala
@@ -1,35 +1,7 @@
 package com.ryanwhittingham.web.query.blog
 
-import com.ryanwhittingham.web.config.Config.config
-import com.zaxxer.hikari.HikariConfig
-import com.zaxxer.hikari.HikariDataSource
-import io.getquill._
-
 object QueryBlog {
-  // Initialize db connection
-  val pgDataSource = new org.postgresql.ds.PGSimpleDataSource()
-  pgDataSource.setUser(config.dbUser)
-  pgDataSource.setDatabaseName(config.dbName)
-  pgDataSource.setPortNumber(config.dbPort)
-
-  if (!config.dbPassword.trim.isEmpty) {
-    pgDataSource.setPassword(config.dbPassword)
-  }
-
-  val dbConfig = new HikariConfig()
-  dbConfig.setDataSource(pgDataSource)
-
-  val ctx = new PostgresJdbcContext(LowerCase, new HikariDataSource(dbConfig))
-
-  case class Blog(
-      id: Int,
-      tstamp: Int,
-      title: String,
-      author: String,
-      contents: String,
-      org_hash: String
-  )
-
+  import com.ryanwhittingham.web.db.Db._
   def queryByTitle(title: String): Option[Blog] = {
     import ctx._
     val blog = ctx.run(query[Blog].filter(_.title == lift(title)))
