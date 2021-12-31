11ba5968d7957e38d2a2964b1f090b8ee73cbf93
Ryan Whittingham <ryanwhittingham89@gmail.com>
2021-12-30 21:15:32 -0800

    handle enter key for search

--- jvm/src/main/resources/ryw.js
+++ jvm/src/main/resources/ryw.js
@@ -1,4 +1,12 @@
+// Add search box handler
 let searchInputElem = document.getElementById("blog-search-button");
 searchInputElem.addEventListener("click", function () {
   ryw.searchHandler("blog-search-input");
+});
+
+// Allow for Enter key as well
+$("#blog-search-input").keyup(function (event) {
+  if (event.keyCode === 13) {
+    $("#blog-search-button").click();
+  }
 });
