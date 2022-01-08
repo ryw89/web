4c6d75bc764c273dcb67df5ae1a6e9e9e146ef78
Ryan Whittingham <ryanwhittingham89@gmail.com>
2022-01-07 19:16:35 -0800

    Added function for redirecting to prev/next blog posts.
    This function will also set HTML buttons with either a
    "prev-blog-post" or "next-blog-post" class as disabled in the case
    that there is no previous or next blog post.

--- js/src/main/scala/web/Main.scala
+++ js/src/main/scala/web/Main.scala
@@ -2,6 +2,7 @@ package com.ryanwhittingham.web
 
 import org.querki.jquery._
 import org.scalajs.dom
+import org.scalajs.dom.ext.Ajax
 import org.scalajs.dom.html.TextArea
 
 import scalajs.js
@@ -58,6 +59,45 @@ object Main {
     val renames = Map("elisp" -> "lisp")
     for ((oldLang, newLang) <- renames) {
       $(s".lang-${oldLang}").attr("class", s"lang-${newLang}")
+    }
+  }
+
+  /** Use public API to get the title of the previous or next blog post
+    * and optionally redirect to its URL. This function will also
+    * disable previous/next buttons if no previous or next blog post
+    * is found. */
+  def redirectToPreviousOrNextTitleUrl(
+      postTitle: String,
+      prev: Boolean,
+      redirect: Boolean = false
+  ) = {
+    // Not the most performant ec for Scala.js, but seemingly OK for
+    // this use case
+    implicit val ec: scala.concurrent.ExecutionContext =
+      scala.concurrent.ExecutionContext.global
+
+    val prevOrNext = if (prev) "prev" else "next"
+    val origin = dom.window.location.origin
+    val url = s"${origin}/api-public/${prevOrNext}-title"
+    val f = Ajax.get(s"${url}/${postTitle}")
+
+    for (value <- f) {
+      val res = value.responseText
+      val titleVal = js.JSON.parse(res).title
+
+      // title will be null if there was no previous/next blog post
+      if (titleVal == null) {
+        // In this case, let's disable the previous or next blog post
+        // buttons found on this page. CSS styling will alter their
+        // appearance.
+        $(s".${prevOrNext}-blog-post").prop("disabled", true)
+      } else {
+        val title = titleVal.toString.toLowerCase.replace(" ", "-")
+        // Redirect if desired
+        if (redirect) {
+          dom.window.location.href = s"${origin}/blog/${title}"
+        }
+      }
     }
   }
 }
